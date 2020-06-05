9fbc800ee4e57a264a18e5c6faaef3c7c737c790
Patrick W Troughton <troughp@gmail.com>
2020-06-05 16:28:51 -0400

    Next Tutorial
    New Project for Next tutorial

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,7 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>[Your Self Introduction]</p>
+        <p>I am a Java Developer starting his career.</p>
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
